f8ef2f4ebc18e164952280413960a7c29bce26fc
Matthew Cavallo <matt.cavallo56@gmail.com>
2023-01-28 20:19:30 -0600

    First

--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,8 @@ namespace TrainingFormsGame3
 {
     public partial class Form1 : Form
     {
+        static bool rectExist = false;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +21,61 @@ namespace TrainingFormsGame3
 
         private void Form1_KeyPress(object sender, KeyPressEventArgs e)
         {
+            if (checkBox1.Checked)
+            {
+                MessageBox.Show($"Form.KeyPress: '{e.KeyChar}' pressed.");
+            }
 
+            switch (e.KeyChar)
+            {
+                case (char)49:
+                case (char)52:
+                case (char)55:
+                    if (checkBox1.Checked)
+                    {
+                        MessageBox.Show($"Form.KeyPress: '{e.KeyChar}' consumed.");
+                    }
+                    e.Handled = true;
+                    break;
+            }
+
+            if (rectExist)
+            {
+                DrawRect(0, 0, 100, 100);
+            }
+            else
+            {
+                DrawRect(0, 0, 100, 100);
+            }
+        }
+
+        public void DrawRect(int posx, int posy, int xsize, int ysize)
+        {
+            var myBrush = new SolidBrush(Color.White);
+
+            // Brush is used to set the color of the Filled Rectangle Color
+            if (rectExist)
+            {
+                myBrush.Color = Color.Red;
+                rectExist = false;
+            }
+            else
+            {
+                myBrush.Color = Color.White;
+                rectExist = true;
+            }
+
+            // Graphics is the object for the Form (GDI+ Drawing Surface)
+            // We then create the graphics which is a reference to the Form1 Object
+            // If we its possible to remove the this statement as we are still in 
+            // context of this Form (This Form being referenced as Form1)
+            Graphics formGraphics;
+            formGraphics = CreateGraphics();
+            formGraphics.FillRectangle(myBrush, posx, posy, xsize, ysize);
+            myBrush.Dispose();
+            formGraphics.Dispose();
+
+            //rectExist = true;
         }
     }
 }
---
+++ RectClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace TrainingFormsGame3
+{
+    public class RectClass
+    {
+        public RectClass(string color = "Red", int posx = 0, int posy = 0, int sizex = 20, int sizey = 20)
+        {
+            if (color == "Red")
+            {
+                this.color = Color.Red;
+            }
+            else
+            {
+                this.color = Color.White;
+            }
+
+            this.posx = posx;
+            this.posy = posy;
+            this.sizex = sizex;
+            this.sizey = sizey;
+        }
+
+        public Color color { get; set; }
+        public int posx { get; set; }
+        public int posy { get; set; }
+        public int sizex { get; set; }
+        public int sizey { get; set; }
+    }
+}
